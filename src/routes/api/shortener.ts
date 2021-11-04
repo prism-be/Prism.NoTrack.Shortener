bcee1e25b4ce0d1bc4176078ce9126e46fa83a57
Simon Baudart <s.baudart@wavenet.be>
2021-11-04 22:30:38 +0100

    Create a CosmosDB

--- src/routes/api/shortener.ts
+++ src/routes/api/shortener.ts
@@ -2,12 +2,78 @@
 import type { LongUrl, ShortUrl } from '$lib/types';
 import type { ResponseHeaders } from '@sveltejs/kit/types/helper';
 import { nanoid } from 'nanoid';
+import storage from 'azure-storage';
 
-export const post = async (request: Request) : Promise<Response> => {
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const post = async (request: Request): Promise<Response> => {
+
+    let headers: ResponseHeaders = {
+
+    }
+
+    if (request.body === null) {
+        return {
+            status: 400,
+            headers
+        };
+    }
+
+    var data: any = request.body;
+
+    if (!data.url.startsWith('https://' || !data.url.startsWith('http://')))
+    {
+        return {
+            status: 400,
+            headers
+        };
+    }
+
+    const connectionString = process.env['COSMOSDB']?.toString();
+
+    if (connectionString == null)
+    {
+        return {
+            status: 500,
+            body: 'The CosmosDB connection string is not found',
+            headers
+        };;
+    }
+
+    const storageClient = storage.createTableService(connectionString);
+
+    var id = nanoid();
+    var partition = id.substring(0, 5);
+    var redirection = {
+        PartitionKey: partition,
+        RowKey: id,
+        LongUrl: data.url,
+        Views: 0
+    };
+
+    storageClient.insertOrMergeEntity("redirections", redirection, function (error, result, response) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        console.log("   insertOrMergeEntity succeeded.");
+    });
+
+    return {
+        status: 200,
+        body: JSON.stringify(getShortUrl(redirection)),
+        headers: headers
+    }
+}
+
+export const get = async (request: Request): Promise<Response> => {
     console.log(request.body);
 
-    var json = JSON.stringify( {
-        "hello": nanoid(16)
+    var json = JSON.stringify({
+        "hello": "world"
     });
 
     let headers: ResponseHeaders = {
@@ -21,20 +87,8 @@ export const post = async (request: Request) : Promise<Response> => {
     }
 }
 
-export const get = async (request: Request) : Promise<Response> => {
-    console.log(request.body);
-
-    var json = JSON.stringify( {
-        "hello": "world"
-    });
-
-    let headers: ResponseHeaders = {
-
-    }
-
+function getShortUrl(redirection: { PartitionKey: string; RowKey: string; LongUrl: any; }): ShortUrl {
     return {
-        status: 200,
-        body: json,
-        headers: headers
-    }
+        url: redirection.RowKey
+    };
 }
