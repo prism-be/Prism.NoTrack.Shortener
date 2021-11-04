1d9c77ebaae0fdec46bbd702301c37d59448fe28
BAUDART Simon <bdrsmn@forem.be>
2021-11-04 10:46:04 +0100

    Create the first part of the project without any externals

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,86 @@
 <script context="module">
-	export const prerender = true;
+    export const prerender = true;
 </script>
 
 <script lang="ts">
-	import Button, { Label, Icon } from '@smui/button';
+    import { Input } from '@smui/textfield';
+    import Paper from '@smui/paper';
+    import Fab from '@smui/fab';
+    import { Icon } from '@smui/common';
 
-	let clicked = 0;
+    let value = '';
 
-	function handleClick(event: CustomEvent | MouseEvent) {
-		event = event as MouseEvent;
-		if (event.button === 0) {
-			clicked++;
-		} else if (event.button === 1) {
-			clicked = 0;
-		}
-	}
+    function doShortener()
+    {
+    }
+
+    function handleKeyDown(event: CustomEvent | KeyboardEvent)
+    {
+        event = event as KeyboardEvent;
+        if (event.key === 'Enter')
+        {
+            doShortener();
+        }
+    }
 </script>
 
-<Button on:mousedown={handleClick}>
-	<Icon class="material-icons">thumb_up</Icon>
-	<Label>Click Me</Label>
-</Button>
-<p class="mdc-typography--body1">
-	{#if clicked}
-		You've clicked the button {clicked} time{clicked === 1 ? '' : 's'}. Middle click the button to
-		reset.
-	{:else}
-		<span class="grayed">You haven't clicked the button.</span>
-	{/if}
-</p>
+<div class="solo-demo-container solo-container">
+    <Paper class="solo-paper" elevation={6}>
+        <Icon class="material-icons">link</Icon>
+        <Input bind:value on:keydown={handleKeyDown} placeholder="Paste your long URL here" class="solo-input"/>
+    </Paper>
+    <Fab on:click={doShortener} disabled={value === ''} color="primary" mini class="solo-fab">
+        <Icon class="material-icons">arrow_forward</Icon>
+    </Fab>
+</div>
+
+<pre class="status">Value: {value}</pre>
+
 
 <style>
-	.grayed {
-		opacity: 0.6;
-	}
+    .solo-demo-container
+    {
+        padding: 36px 18px;
+        background-color: #f8f8f8;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+    }
+
+    .solo-container
+    {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    * :global(.solo-paper)
+    {
+        display: flex;
+        align-items: center;
+        flex-grow: 1;
+        max-width: 600px;
+        margin: 0 12px;
+        padding: 0 12px;
+        height: 48px;
+    }
+
+    * :global(.solo-paper > *)
+    {
+        display: inline-block;
+        margin: 0 12px;
+    }
+
+    * :global(.solo-input)
+    {
+        flex-grow: 1;
+    }
+
+    * :global(.solo-input::placeholder)
+    {
+        opacity: 0.6;
+    }
+
+    * :global(.solo-fab)
+    {
+        flex-shrink: 0;
+    }
 </style>
