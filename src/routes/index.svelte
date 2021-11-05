9beca32e14c35aa9d2c80cc6563821a74ca2d384
Simon <simon@contact-me.be>
2021-11-05 14:57:44 +0100

    Change for direct link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -126,7 +126,7 @@
 
 			<Subtitle class="paper-subtitle">Another way to support ?</Subtitle>
 			Yes, of course ! This is an open source project, you can fork it and add the feature you want.
-			This is stored in GitHub : <a href="https://s.notrack.cloud/r/zeUL5Bears5ME7-NJl7aQ">Prism.NoTrack.Shortener</a>.
+			This is stored in GitHub : <a href="https://github.com/simonbaudart/Prism.NoTrack.Shortener">Prism.NoTrack.Shortener</a>.
 
 			<Subtitle class="paper-subtitle">Bugs ? Support ?</Subtitle>
 			This project is made on personal time, with the use of beta version of <a href="https://kit.svelte.dev/">SvelteKit</a>. there can be bugs, I can take time to fix them. <br />
