ddd37759722df82c4cba4385e2db116951d95386
Simon <simon@contact-me.be>
2021-12-08 16:06:21 +0100

    refactor: remove unused code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,44 +1,15 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+    export const prerender = true;
 </script>
 
 <script lang="ts">
-	import About from '$lib/components/About.svelte';
-import Shorten from '$lib/components/Shorten.svelte';
-
-	import type { LongUrl, ShortUrl } from '$lib/types';
-
-	let value = '';
-	let shortened = '';
-
-	function doShortener() {
-		const longUrl: LongUrl = {
-			url: value
-		};
-
-		fetch('/api/shortener', {
-			method: 'POST',
-			body: JSON.stringify(longUrl),
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8'
-			}
-		}).then((response) => {
-			response.json().then((shortUrl: ShortUrl) => {
-				shortened = shortUrl.url;
-			});
-		});
-	}
-
-	function handleKeyDown(event: any) {
-		if (event.key === 'Enter') {
-			doShortener();
-		}
-	}
+    import About from '$lib/components/About.svelte';
+    import Shorten from '$lib/components/Shorten.svelte';
 </script>
 
 <svelte:head>
-	<title>Shorten your URL - NoTrack : For a private, ad and tracking free web</title>
+    <title>Shorten your URL - NoTrack : For a private, ad and tracking free web</title>
 </svelte:head>
 
-<Shorten />
-<About />
+<Shorten/>
+<About/>
