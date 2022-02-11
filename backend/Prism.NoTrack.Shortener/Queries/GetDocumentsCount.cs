8149c9bc606878c381da1a5a0dfd2bc37572a6f2
Simon <simon@contact-me.be>
2022-02-11 11:31:27 +0100

    feat(monitoring): Add HealthChecks on Database

---
+++ backend/Prism.NoTrack.Shortener/Queries/GetDocumentsCount.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="GetDocumentsCount.cs" company="Prism">
+//  Copyright (c) Prism. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Prism.NoTrack.Shortener.Queries;
+
+using LiteDB;
+
+using MediatR;
+
+using Microsoft.Extensions.Options;
+
+using Prism.NoTrack.Shortener.Model;
+using Prism.NoTrack.Shortener.Options;
+
+public record GetDocumentsCount : IRequest<int>;
+
+public class GetDocumentsCountHandler : IRequestHandler<GetDocumentsCount, int>
+{
+    private readonly ShortenerConfiguration configuration;
+
+    public GetDocumentsCountHandler(IOptions<ShortenerConfiguration> configuration)
+    {
+        this.configuration = configuration.Value;
+    }
+
+    public Task<int> Handle(GetDocumentsCount request, CancellationToken cancellationToken)
+    {
+        using var database = new LiteDatabase(this.configuration.ConnectionString);
+        var collection = database.GetCollection<Redirection>("customers");
+
+        return Task.FromResult(collection.Count());
+    }
+}
