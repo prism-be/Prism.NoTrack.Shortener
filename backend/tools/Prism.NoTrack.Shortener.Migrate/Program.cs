b83288f5084322bcfc948752bfe9eccc025c8f7f
Simon <simon@contact-me.be>
2022-02-16 15:40:14 +0100

    chore: Create a small tool to migrate the old CosmosDB database

---
+++ backend/tools/Prism.NoTrack.Shortener.Migrate/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LiteDB;
+
+using Microsoft.Azure.Cosmos;
+
+using Prism.NoTrack.Shortener.Model;
+
+var cosmosDbConnectionString = Environment.GetEnvironmentVariable("COSMOSDB_CONNECTIONSTRING");
+var liteDbConnectionString = Environment.GetEnvironmentVariable("LITEDB_CONNECTIONSTRING");
+
+using var cosmosClient = new CosmosClient(cosmosDbConnectionString);
+var database = cosmosClient.GetDatabase("shortener");
+var container = database.GetContainer("redirections");
+var queryDefinition = new QueryDefinition("SELECT * FROM c");
+
+var iterator = container.GetItemQueryIterator<dynamic>(queryDefinition);
+
+var results = new List<dynamic>();
+while (iterator.HasMoreResults)
+{
+    var result = await iterator.ReadNextAsync();
+    results.AddRange(result.Resource);
+}
+
+foreach (var result in results)
+{
+    using var liteDatabase = new LiteDatabase(liteDbConnectionString);
+    var collection = liteDatabase.GetCollection<Redirection>("customers");
+
+    var redirection = new Redirection(result.id.ToString(), result.longUrl.ToString());
+
+    collection.Insert(redirection);
+    collection.EnsureIndex(x => x.Id);
+}
